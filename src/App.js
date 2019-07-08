4aac43ba1ed13f5b52a822a101518bd28eefea32
Biplav Sharma <nifty@Biplavs-MacBook-Pro.local>
2019-07-08 08:01:41 +0545

    Add lifecycle methods to component

--- src/App.js
+++ src/App.js
@@ -2,26 +2,45 @@ import React, { Component } from 'react'
 import MainLayout from './MainLayout';
 
 export default class App extends Component {
-    state = {
-        name: 'Biplav'
+    constructor(props){
+        super(props);
+        this.state= {
+            name: 'Biplav'
+        }
+        console.log('constructor');
     }
-    abc = () => {
+    componentWillMount(){
+        console.log('componentWill Mount');
+    }
+    componentDidMount(){
+        console.log('component Did Mount')
+    }
+    componentWillUpdate(){
+        console.log('componentWillUpdate');
+    }
+    componentDidUpdate(){
+        alert('press ok to continue')
+        console.log('component did update')
+    }
+    componentWillUnmount(){
+        console.log('component will unmount')
+    }
+    handleChange = () => {
+        console.log('name changed')
         this.setState({
             name: 'Maoist'
         }, ()=>{
-            // console.log('name changed')
         })
     }
     render() {
+        console.log('render')
         let hello = "earth"
         return (
             <MainLayout header="header1" footer="footer">
-                {
-                    console.log('hello')
-                }
                 <h1>{ this.props.data }</h1>
                 <h2>{ this.state.name }</h2>
-                <button onClick={this.abc}>Change My Name</button>
+                <button onClick={this.handleChange}>Change My Name</button>
+                <a href="https://github.com">Git</a>
             </MainLayout>
         )
     }
